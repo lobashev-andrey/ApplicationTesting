60f11372fb162e88e9c6ce59e38ba4f216f02e75
lobashev-andrey <lobashev.andrey@mail.ru>
2024-03-27 19:18:18 +0300

    Initial commit

---
+++ fibonacci/src/test/java/com/skillbox/fibonacci/FibonacciCalculatorTest.java
@@ -0,0 +1,15 @@
+package com.skillbox.fibonacci;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+public class FibonacciCalculatorTest {
+
+
+
+
+
+}
