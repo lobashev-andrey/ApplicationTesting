d994c897af2073fe19cfd1137258fdfe22f1e950
lobashev-andrey <lobashev.andrey@mail.ru>
2024-03-28 09:55:17 +0300

    unit tests for FibonacciCalculator added

--- fibonacci/src/test/java/com/skillbox/fibonacci/FibonacciCalculatorTest.java
+++ fibonacci/src/test/java/com/skillbox/fibonacci/FibonacciCalculatorTest.java
@@ -1,15 +1,46 @@
 package com.skillbox.fibonacci;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FibonacciCalculatorTest {
+    FibonacciCalculator calculator = new FibonacciCalculator();
+    @Test
+    @DisplayName("test get when index less than 1")
+    public void givenIndexLessThanOne_testGetFibonacciNumber() {
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> calculator.getFibonacciNumber(0));
+        assertEquals("Index should be greater or equal to 1", ex.getMessage());
+    }
 
+    @ParameterizedTest
+    @DisplayName("test get when index 1 or 2")
+    @ValueSource(ints = {1, 2})
+    public void givenIndex1or2_testGetFibonacciNumber(int givenIndex) {
+        assertEquals(1, calculator.getFibonacciNumber(givenIndex));
+    }
 
+    @ParameterizedTest
+    @CsvSource({
+            "5, 5",
+            "10, 55",
+            "30, 832040"})
+    @DisplayName("test get when index > 2")
+   public void givenIndexGraterThen2_testGetFibonacciNumber(int givenIndex, int expected) {
+        assertEquals(expected, calculator.getFibonacciNumber(givenIndex));
+    }
 
-
-
+    // when data is too big
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data_for_FibonacciCalculatorTest.csv")
+    @DisplayName("test get when index > 2")
+    public void givenIndexGraterThen2_testGetFibonacciNumber_withCSVFile(int givenIndex, int expected) {
+        assertEquals(expected, calculator.getFibonacciNumber(givenIndex));
+    }
 }
