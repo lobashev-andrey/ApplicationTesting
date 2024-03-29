578b9c5f5c74bcda81a4e6eebb7ded97dcb47691
lobashev-andrey <lobashev.andrey@mail.ru>
2024-03-29 11:09:46 +0300

    integration tests with mockMvc FibonacciControllerTest added

--- fibonacci/src/test/java/com/skillbox/fibonacci/FibonacciControllerTest.java
+++ fibonacci/src/test/java/com/skillbox/fibonacci/FibonacciControllerTest.java
@@ -1,9 +1,15 @@
 package com.skillbox.fibonacci;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -12,4 +18,32 @@ class FibonacciControllerTest extends PostgresTestContainerInitializer {
     @Autowired
     private MockMvc mockMvc;
 
+    @Test
+    @DisplayName("when find by index > 0 then status ok and return proper value")
+    public void whenFindByIndexGraterThan_0_thenReturnProperValue() throws Exception {
+
+        String actual = mockMvc.perform(get("/fibonacci/10"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        String expected = "{\"index\":10,\"value\":55}";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("when find by index < 1 then status 400 and message")
+    public void whenFindByIndexLessThan_1_thenStatus_400() throws Exception {
+
+        var response = mockMvc.perform(get("/fibonacci/-8"))
+                .andExpect(status().isBadRequest())
+                .andReturn()
+                .getResponse();
+
+        String actual = response.getContentAsString();
+        String expected = "Index should be greater or equal to 1";
+
+        assertEquals(expected, actual);
+    }
 }
